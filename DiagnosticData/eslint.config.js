582878e9adfec810354a15bd1355f77b443c3929
guyrenny <guy.renny@coralogix.com>
2025-06-12 11:48:09 +0300

    remvoe .ts from the ignor file

--- DiagnosticData/eslint.config.js
+++ DiagnosticData/eslint.config.js
@@ -4,7 +4,6 @@ import tsParser from "@typescript-eslint/parser";
 export default [
 	{
 		ignores: ["node_modules", "dist"],
-		files: ["**/*.ts", "**/*.tsx"],
 		languageOptions: {
 			parser: tsParser,
 		},
