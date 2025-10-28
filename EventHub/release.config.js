958bbd22c4d632345a18a3fe2b5916b10c343d0d
vjofusha <vjosa.fusha@coralogix.com>
2025-10-28 13:27:11 +0200

    feat: add github release for eventhub abd blob to otel

--- EventHub/release.config.js
+++ EventHub/release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branches: ['azure-github-release*'],
+    branches: ['master'],
     tagFormat: 'EventHub-v${version}',
     plugins: [
       '@semantic-release/commit-analyzer',
@@ -9,7 +9,7 @@ module.exports = {
         successCmd: 'echo ${nextRelease.version} > .release_version'
       }],
       ['@semantic-release/git', {
-        assets: ['CHANGELOG.md', '.release_version'],
+        assets: ['CHANGELOG.md', '.release_version', "package.json"],
         message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
       }],
       ['@semantic-release/github', {
