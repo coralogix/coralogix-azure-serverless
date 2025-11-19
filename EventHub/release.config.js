db370215992edda395bba9af7ee29f868942d026
vjofusha <vjosa.fusha@coralogix.com>
2025-11-19 18:47:59 +0100

    feat!: migrate ARM template to use GitHub releases
    BREAKING CHANGE: ARM template now deploys from GitHub releases v3.0.0 instead of S3 bucket (v2.0.2). This includes the OpenTelemetry SDK rewrite, updated Coralogix ingress endpoints, EventHub consumer group support, and Node.js 22 runtime.

--- EventHub/release.config.js
+++ EventHub/release.config.js
@@ -4,13 +4,8 @@ module.exports = {
     plugins: [
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
-      ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
       ['@semantic-release/exec', {
         successCmd: 'echo ${nextRelease.version} > .release_version'
-      }],
-      ['@semantic-release/git', {
-        assets: ['CHANGELOG.md', '.release_version', "package.json"],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
       }],
       ['@semantic-release/github', {
         assets: [
